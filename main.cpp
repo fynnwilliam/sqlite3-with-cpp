dfee4c6a5ed57c1dfa9eed5bf01ec99382ccd459
Fynn William Christian <fynnwilliam@gmail.com>
2021-06-22 20:48:39 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,20 +4,20 @@
 int main()
 {
     std::cout << "opening database...\n";
-	open_database("example.db");
+    open_database("example.db");
 
     std::cout << "creating tables...\n";
-	create_table(table);
+    create_table(table);
 
     std::cout << "inserting and displaying values...\n";
-	insert_values(values);
+    insert_values(values);
     display_data(all);
 
     std::cout << "deleting a record...\n";
-	delete_data(alien);
+    delete_data(alien);
 
     std::cout << "updating some fields...\n";
-	update_data(fields);
+    update_data(fields);
     display_data(after_update);
 	
     std::cout << "displaying selected columns...\n";
